5433e387573242ccd04122135bc6a01ff6ceb185
Raulalberto1 <raul.alberto.91@gmail.com>
2024-08-13 16:40:37 -0600

    first commit

---
+++ src/main/java/com/example/JavaGhaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGhaApplication.class, args);
+	}
+
+}
